0cf5db3f29ed0983f7a1877af536b0288d955449
Shalini Mittal <shalini06.mittal@gmail.com>
2023-01-10 15:04:35 +0530

    first commit

---
+++ trial/demo.py
@@ -0,0 +1,14 @@
+a = int(input('enter a number'))
+b = int(input('enter a number'))
+if a == 10:
+    print('i am 10')
+    if a>b:
+        print('a is greater')
+    else:
+        print('b is greater')
+elif a==20: # else if
+    print('i ma 20')
+else:
+    print('Dont know')
+
+print('I am done')
